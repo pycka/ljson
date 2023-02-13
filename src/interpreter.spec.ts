33b49350827b8484374a1e2a4f4833954c78810b
Radek Pycka <rpycka@gmail.com>
2023-02-13 23:33:46 +0100

    refactor: aggregate context and variables in ExecutionContext, add access to last expression's value for chaining

--- src/interpreter.spec.ts
+++ src/interpreter.spec.ts
@@ -1,10 +1,14 @@
 import assert from 'assert';
-import { execute } from './interpreter';
+import { execute, Variables } from './interpreter';
 import { Script } from './language.model';
 
 describe('execute()', () => {
+  function runExecute(script: Script, context?: unknown, variables?: Variables): unknown {
+    return execute(script, { this: context, variables });
+  }
+  
   it('GIVEN empty list of expressions SHOULD return undefined', () => {
-    assert.strictEqual(execute([]), undefined);
+    assert.strictEqual(runExecute([]), undefined);
   });
 
   it('GIVEN 2 commands SHOULD return value of the last one', () => {
@@ -12,26 +16,26 @@ describe('execute()', () => {
       ['get', 'a'],
       ['get', 'b'],
     ];
-    assert.strictEqual(execute(script, null, { a: 1, b: 2 }), 2);
+    assert.strictEqual(runExecute(script, null, { a: 1, b: 2 }), 2);
   });
 
   describe('GET expression', () => {
     describe('GIVEN single instruction is passed to `execute`', () => {
       it('WHEN passed "this" as an expression SHOULD return context object', () => {
         const context = {};
-        assert.strictEqual(execute(['get', 'this'], context, {}), context);
+        assert.strictEqual(runExecute(['get', 'this'], context, {}), context);
       });
 
       it('WHEN passed "a" as an expression SHOULD return "a" variable value', () => {
-        assert.strictEqual(execute(['get', 'a'], null, { a: 5 }), 5);
+        assert.strictEqual(runExecute(['get', 'a'], null, { a: 5 }), 5);
       });
 
       it('WHEN passed "this.a" as an expression SHOULD return value of the "a" property of the context object', () => {
-        assert.strictEqual(execute(['get', 'this.a'], { a: 10 }, { a: 5 }), 10);
+        assert.strictEqual(runExecute(['get', 'this.a'], { a: 10 }, { a: 5 }), 10);
       });
 
       it('WHEN ["get", "this.a"] expression is passed SHOULD return value of the variable named by context "a" property', () => {
-        assert.strictEqual(execute(['get', ['get', 'this.a']], { a: 'b' }, { b: 5 }), 5);
+        assert.strictEqual(runExecute(['get', ['get', 'this.a']], { a: 'b' }, { b: 5 }), 5);
       });
     });
   });
@@ -52,44 +56,44 @@ describe('execute()', () => {
 
     it('WHEN raw value is set on the context object SHOULD update context object and return value', () => {
       const newValue = 5;
-      assert.strictEqual(execute(['set', 'this.newProperty', newValue], context), newValue);
+      assert.strictEqual(runExecute(['set', 'this.newProperty', newValue], context), newValue);
       assert.strictEqual(context.newProperty, newValue);
     });
 
     it('WHEN raw value is set on the nested context object SHOULD update proper object and return new value', () => {
       const newValue = { ala: 'ma kota' };
-      assert.strictEqual(execute(['set', 'this.nestedContext.newProperty', newValue], context), newValue);
+      assert.strictEqual(runExecute(['set', 'this.nestedContext.newProperty', newValue], context), newValue);
       assert.strictEqual(context.nestedContext.newProperty, newValue);
     });
 
     it('WHEN raw value is set on the variables object SHOULD update variable and return new value', () => {
       const newValue = true;
-      assert.strictEqual(execute(['set', 'newProperty', newValue], context, variables), newValue);
+      assert.strictEqual(runExecute(['set', 'newProperty', newValue], context, variables), newValue);
       assert.strictEqual(variables.newProperty, newValue);
     });
 
     it('WHEN raw value is set on the nested variable object SHOULD update property on the variable and return new value', () => {
       const newValue = null;
-      assert.strictEqual(execute(['set', 'nestedVariable.newProperty', newValue], context, variables), newValue);
+      assert.strictEqual(runExecute(['set', 'nestedVariable.newProperty', newValue], context, variables), newValue);
       assert.strictEqual(variables.nestedVariable.newProperty, newValue);
     });
 
     it('WHEN empty array is set on the variable object SHOULD update property with undefined (empty expression result)', () => {
       const newValue = undefined;
-      assert.strictEqual(execute(['set', 'newProperty', []], context, variables), newValue);
+      assert.strictEqual(runExecute(['set', 'newProperty', []], context, variables), newValue);
       assert.strictEqual(variables.newProperty, newValue);
     });
 
     describe('GIVEN expression is used as value source', () => {
       it('WHEN ["value", []] is passed SHOULD update variable with an empty array', () => {
         const newValue: unknown[] = [];
-        assert.strictEqual(execute(['set', 'this.newProperty', ['value', newValue]], context), newValue);
+        assert.strictEqual(runExecute(['set', 'this.newProperty', ['value', newValue]], context), newValue);
         assert.strictEqual(context.newProperty, newValue);
       });
 
       it('WHEN ["get", "oldVariable"] is passed SHOULD update variable with value from existing variable', () => {
         const script: Script = ['set', 'this.newProperty', ['get', 'oldVariable']];
-        assert.strictEqual(execute(script, context, variables), 'value');
+        assert.strictEqual(runExecute(script, context, variables), 'value');
         assert.strictEqual(context.newProperty, 'value');
       });
     });
@@ -98,7 +102,7 @@ describe('execute()', () => {
   describe('VALUE expression', () => {
     it('WHEN passed instruction-like argument SHOULD return raw value', () => {
       const argument = ['get', 'a'];
-      assert.strictEqual(execute(['value', argument], {}, {}), argument);
+      assert.strictEqual(runExecute(['value', argument], {}, {}), argument);
     });
   });
 
@@ -134,19 +138,19 @@ describe('execute()', () => {
     });
 
     it('SHOULD invoke method on the context object', () => {
-      assert.equal(execute(['call', 'this.contextMethod'], context, variables), 1);
+      assert.equal(runExecute(['call', 'this.contextMethod'], context, variables), 1);
     });
 
     it('SHOULD invoke nested method on the context object', () => {
-      assert.equal(execute(['call', 'this.nestedContext.nestedMethod'], context, variables), 2);
+      assert.equal(runExecute(['call', 'this.nestedContext.nestedMethod'], context, variables), 2);
     });
 
     it('SHOULD invoke function without context object', () => {
-      assert.equal(execute(['call', 'functionOne'], context, variables), undefined);
+      assert.equal(runExecute(['call', 'functionOne'], context, variables), undefined);
     });
 
     it('SHOULD invoke nested method on the variable object', () => {
-      assert.equal(execute(['call', 'nestedVariable.nestedMethod'], context, variables), 4);
+      assert.equal(runExecute(['call', 'nestedVariable.nestedMethod'], context, variables), 4);
     });
   });
 
@@ -170,12 +174,12 @@ describe('execute()', () => {
 
     it('SHOULD return a function', () => {
       const script: Script = ['lambda', ['one'], []];
-      assert.equal(typeof execute(script), 'function');
+      assert.equal(typeof runExecute(script), 'function');
     })
     ;
     it('SHOULD return a function returning 5', () => {
       const script: Script = ['lambda', ['one'], [['value', 5]]];
-      assert.equal((execute(script) as (() => number))(), 5);
+      assert.equal((runExecute(script) as (() => number))(), 5);
     });
 
     it('SHOULD subscribe to a context method returning promise and return resolved value', (done) => {
@@ -193,7 +197,7 @@ describe('execute()', () => {
           ]
         ]
       ];
-      execute(script, context, variables);
+      runExecute(script, context, variables);
       assert.doesNotReject(() => variables.promise);
     });
 
@@ -211,7 +215,7 @@ describe('execute()', () => {
         ['set', 'promise', ['call', 'getRejectingPromise']],
         ['call', 'promise.then', [['get', 'resolve'], ['get', 'fail']]],
       ];
-      execute(script, context, variables);
+      runExecute(script, context, variables);
     });
 
     it('SHOULD subscribe to a function returning resolved promise, assert validity and complete the test', (done) => {
@@ -229,7 +233,25 @@ describe('execute()', () => {
         ['set', 'promise', ['call', 'promise.then', [['get', 'onResolve']]]],
         ['call', 'promise.catch', [['get', 'done']]],
       ];
-      execute(script, context, variables);
+      runExecute(script, context, variables);
+    });
+
+    it('SHOULD chain calls using $ variable', (done) => {
+      variables.done = done;
+      variables.assert = assert;
+      const script: Script = [
+        ['set', 'onResolve',
+          ['lambda', ['resolvedValue'], [
+            ['call', 'assert.equal', [['get', 'resolvedValue[0]'], ['value', 'get']]],
+            ['call', 'assert.equal', [['get', 'resolvedValue[1]'], ['value', 'this']]],
+            ['call', 'done']
+          ]]
+        ],
+        ['call', 'this.getResolvedPromise', [['value', ['get', 'this']]]],
+        ['call', '$.then', [['get', 'onResolve']]],
+        ['call', '$.catch', [['get', 'done']]],
+      ];
+      runExecute(script, context, variables);
     });
   });
 
--- src/interpreter.ts
+++ src/interpreter.ts
@@ -4,9 +4,14 @@ import { Expression, RawValue, Script } from './language.model';
 export type Variables = Record<string, any>;
 export type ExtractExpressionName<T> = T extends [infer C, ...unknown[]] ? C : T;
 export type CommandNames = ExtractExpressionName<Expression>;
+export interface ExecutionContext {
+  this: unknown;
+  variables: Variables;
+  lastValue?: unknown;
+}
 
 const rawValues = new WeakSet();
-const implementations: Record<CommandNames, (context: unknown, variables: Variables, ...params: any[]) => unknown> = {
+const implementations: Record<CommandNames, (context: ExecutionContext, ...params: any[]) => unknown> = {
   /**
    * Invoke function or method.
    * If the expression looks like a method invocation then parent expression will be used to get the context object.
@@ -14,52 +19,59 @@ const implementations: Record<CommandNames, (context: unknown, variables: Variab
    *
    * @todo add 3rd parameter: context
    */
-  call: (context: unknown, variables: Variables, functionExpression: string | Expression, params: (RawValue | Expression)[] = []): unknown => {
+  call: (context: ExecutionContext, functionExpression: string | Expression, params: (RawValue | Expression)[] = []): unknown => {
     const callPathOrFunction = typeof functionExpression !== 'string'
-      ? execute(functionExpression, context, variables) as (string | ((...args: unknown[]) => unknown))
+      ? execute(functionExpression, context) as (string | ((...args: unknown[]) => unknown))
       : functionExpression;
-    let callContext = context;
+    let callContext = context.this;
     let functionToCall: ((...args: unknown[]) => unknown);
     if (typeof callPathOrFunction === 'function') {
       functionToCall = callPathOrFunction;
     } else {
-      functionToCall = getValue(callPathOrFunction, context, variables) as (...args: unknown[]) => unknown;
+      functionToCall = getValue(callPathOrFunction, context) as (...args: unknown[]) => unknown;
       const pathChunks = toPath(callPathOrFunction);
       if (pathChunks.length > 1) {
-        callContext = getValue(pathChunks.slice(0, -1).join('.'), context, variables);
+        callContext = getValue(pathChunks.slice(0, -1).join('.'), context);
       }
     }
     const callParams = params.map((param) =>
       isRawValue(param)
         ? param
-        : execute(param, context, variables)
+        : execute(param, context)
     );
+    if (typeof functionToCall !== 'function') {
+      throw new Error(`expected "${callPathOrFunction}" expression to evaluate to a function`);
+    }
     return functionToCall.apply(callContext, callParams);
   },
 
   /**
    * Get value from the context object or available variables.
    */
-  get: (context: unknown, variables: Variables, expression: string | Script): unknown => {
+  get: (context: ExecutionContext, expression: string | Script): unknown => {
     const expressionToRead = typeof expression === 'string'
       ? expression
-      : execute(expression, context, variables);
+      : execute(expression, context);
     if (typeof expressionToRead !== 'string') {
-      throw `Execution error: expected string in GET ${expression}`;
+      throw new Error(`expected string in GET ${expression}`);
     }
-    return getValue(expressionToRead, context, variables);
+    return getValue(expressionToRead, context);
   },
 
   /**
    * Create function object. Useful when API expects a callback.
    */
-  lambda: (context: unknown, variables: Variables, paramNames: string[], script: Script): unknown => {
+  lambda: (context: ExecutionContext, paramNames: string[], script: Script): unknown => {
     return function (...args: unknown[]): unknown {
-      const scopedVariables = Object.create(variables);
+      const scopedVariables = Object.create(context.variables);
       paramNames.forEach((paramName, index) => {
         scopedVariables[paramName] = args[index];
       });
-      return execute(script, context, scopedVariables);
+      const newContext: ExecutionContext = {
+        ...context,
+        variables: scopedVariables,
+      };
+      return execute(script, newContext);
     };
   },
 
@@ -67,22 +79,22 @@ const implementations: Record<CommandNames, (context: unknown, variables: Variab
    * Update value of a variable or context object property.
    * @todo mutate prototype chain object that contain actual values instead of the topmost one (simulating closures)
    */
-  set: (context: any, variables: Variables, receiver: string, valueExpression: Script): unknown => {
+  set: (context: ExecutionContext, receiver: string, valueExpression: Script): unknown => {
     const valueToSet = isRawValue(valueExpression)
       ? valueExpression
-      : execute(valueExpression, context, variables);
+      : execute(valueExpression, context);
     if (receiver.startsWith('this.')) {
-      set(context, receiver.substring(5), valueToSet);
+      set(context.this as object, receiver.substring(5), valueToSet);
     } else {
-      set(variables, receiver, valueToSet);
+      set(context.variables, receiver, valueToSet);
     }
     return valueToSet;
   },
 
   /**
-   * Returns passed value without any processing. Escapes any interpolation.
+   * Returns passed value without any processing. Escapes any interpretation of the expression passed.
    */
-  value: (context: unknown, variables: Variables, expression: unknown): unknown => {
+  value: (context: ExecutionContext, expression: unknown): unknown => {
     if (expression && typeof expression === 'object') {
       rawValues.add(expression);
     }
@@ -98,22 +110,35 @@ function isSingleExpression(script: Script): script is Expression {
   return script[0] && !Array.isArray(script[0]);
 }
 
-function getValue(expression: string, context: unknown, variables: Variables): unknown {
-  if (expression === 'this') {
-    return context;
-  } else if (expression.startsWith('this.')) {
-    return get(context, expression.substring(5));
+/**
+ * @todo get rid of lodash
+ */
+function getValue(expression: string, context: ExecutionContext): unknown {
+  if (expression === 'this' || expression.startsWith('this.') || expression.startsWith('this[')) {
+    return get(context, expression)
+  } else if (expression === '$') {
+    return context.lastValue;
+  } if (expression.startsWith('$.') || expression.startsWith('$[')) {
+    return get(context.lastValue, expression.substring(2));
   } else {
-    return get(variables, expression);
+    return get(context.variables, expression);
   }
 }
 
-export function execute(script: Script, context?: unknown, variables: Variables = {}): unknown {
+export function execute(script: Script, executionContext: Partial<ExecutionContext> = {}): unknown {
+  const contextWithDefaults: ExecutionContext = {
+    this: executionContext.this ?? {},
+    variables: executionContext.variables ?? {},
+  };
   const expressionList: Expression[] = isSingleExpression(script)
     ? [script]
     : script;
-  return expressionList.reduce(
-    (previousValue: unknown, [command, ...params]): unknown => implementations[command](context, variables, ...params),
+  contextWithDefaults.lastValue = expressionList.reduce(
+    (lastValue: unknown, [command, ...params]): unknown => {
+      contextWithDefaults.lastValue = lastValue;
+      return implementations[command](contextWithDefaults, ...params);
+    },
     undefined
   );
+  return contextWithDefaults.lastValue;
 }
